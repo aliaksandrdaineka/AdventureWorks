0b101b628fb5f71db88a3185ccd90241cb58dd76
daynekoas <daynekoas@gmail.com>
2019-11-03 02:40:27 +0300

    Added ProductsService

---
+++ AdventureWorks.Services/IProductsService.cs
@@ -0,0 +1,18 @@
+﻿using AdventureWorks.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventureWorks.Services
+{
+    public interface IProductsService
+    {
+        Product GetById(int id);
+        Product Create(Product product);
+        Product Update(Product product);
+
+        void Remove(Product product);
+
+        IEnumerable<Product> GetAll();
+    }
+}
---
+++ AdventureWorks.Services/ProductsService.cs
@@ -0,0 +1,59 @@
+﻿using AdventureWorks.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventureWorks.Services
+{
+    public class ProductsService : IProductsService
+    {
+        public Product Create(Product product)
+        {
+            using (var db = new DataContext())
+            {
+                db.Product.Add(product);
+                db.SaveChanges();
+
+                return product;
+            }
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            using (var db = new DataContext())
+            {
+                return db.Product.ToList();
+            }
+        }
+
+        public Product GetById(int id)
+        {
+            using (var db = new DataContext())
+            {
+                return db.Product.Find(id);
+            }
+        }
+
+        public void Remove(Product product)
+        {
+            using (var db = new DataContext())
+            {
+                db.Product.Remove(product);
+                db.SaveChanges();
+            }
+        }
+
+        public Product Update(Product product)
+        {
+            using (var db = new DataContext())
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return product;
+            }
+        }
+    }
+}
